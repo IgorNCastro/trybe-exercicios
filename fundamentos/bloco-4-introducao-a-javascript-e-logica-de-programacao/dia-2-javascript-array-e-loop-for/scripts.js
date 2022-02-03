fc19e6888326b5de11be476f0363c4545f04126f
Igor Castro <ncastro.igor@gmail.com>
2022-02-03 19:36:08 -0300

    Exercicios do dia finalizados

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,86 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercicio 01
+// for (let number of numbers) {
+//    console.log(number);
+// }
+
+
+// Exercicio 02
+// let sumNumber = 0;
+// for (let number of numbers) {
+//     sumNumber += number;
+// }
+// console.log(sumNumber);
+
+
+// Exercicio 03
+// let sumNumber = 0;
+// for (let number of numbers) {
+//     sumNumber += number;
+// }
+// let mediaAritmetica = (sumNumber / numbers.length);
+// console.log(mediaAritmetica);
+
+
+// Exercicio 04
+// let sumNumber = 0;
+// for (let number of numbers) {
+//     sumNumber += number;
+// }
+// let mediaAritmetica = (sumNumber / numbers.length);
+// if (mediaAritmetica > 20) {
+//     console.log("valor maior que 20");
+// }
+// else if (mediaAritmetica <= 20){
+//     console.log("valor menor ou igual a 20");
+// }
+
+
+// Exercicio 05
+// let maiorValor = 0;
+// for (let number of numbers) {
+//     if (number > maiorValor) {
+//         maiorValor = number;
+//     }
+// }
+// console.log(maiorValor);
+
+
+// Exercicio 06
+// let numerosImpar = 0;
+// for (let number of numbers) {
+//     if (number % 2 !== 0) {
+//         numerosImpar += 1;
+//     }
+// }
+// if (numerosImpar > 0) {
+//     console.log(numerosImpar)
+// } else {
+//     console.log("nenhum valor ímpar encontrado")
+// }
+
+
+// Exercicio 07
+// let menorValor = 1000;
+// for (let number of numbers) {
+//     if (number < menorValor) {
+//         menorValor = number;
+//     }
+// }
+// console.log(menorValor);
+
+// Exercicio 08
+// array = [];
+// for (let index = 1; index <= 25; index += 1) {
+//     array.push(index);
+// }
+// console.log(array);
+
+
+// Exercicio 09
+// let arrayDivisao = 0;
+// for (let index of array) {
+//     arrayDivisao = (index / 2);
+//     console.log(arrayDivisao);
+// }
