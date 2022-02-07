ee3e979b124c6de8334a16b3e480a7c4335b63e9
Igor Castro <ncastro.igor@gmail.com>
2022-02-07 20:52:43 -0300

    Primeira parte dos exercicios finalizados

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -0,0 +1,75 @@
+// NOVO OBJETO
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// Exercicio 01
+// console.log('Bem-vinda, ' + info.personagem);
+
+
+// Exercicio 02
+// info.recorrente = 'Sim';
+// console.table(info);
+
+
+// Exercicio 03
+// for (let key in info) {
+//     console.log(key);
+// };
+
+
+// Exercicio 04
+// for (let key in info) {
+//     console.log(info[key]);
+// };
+
+
+// Exercicio 05
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+// };
+
+// console.log(info.personagem + ' e ' + info2.personagem);
+// console.log(info.origem + ' e ' + info2.origem);
+// console.log(info.nota + ' e ' + info2.nota);
+// if (info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
+//     console.log('Ambos recorrentes');
+// };
+
+
+// NOVO OBJETO
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+//     ],
+// };
+
+// Exercicio 06
+// console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor['livrosFavoritos'][0]['titulo'] + '"');
+
+
+// Exercicio 07
+// leitor.livrosFavoritos.push(
+//     {
+//         titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//         autor: 'JK Rowling',
+//         editor: 'Rocco',
+//     },
+// );
+// console.log(leitor);
+
+
+// Exercicio 08
+// console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.')
