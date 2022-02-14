f9cddb45a1666a0e362abc32d1163d422bc740b5
Igor Castro <ncastro.igor@gmail.com>
2022-02-14 19:58:31 -0300

    Commit 02 - Resolução exercicio 02

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/scripts.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/scripts.js
@@ -14,6 +14,7 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
   
 // Escreva seu código abaixo.
+// Exercicio 01
 let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 function createDaysOfTheMonth() {
@@ -43,3 +44,14 @@ function createDaysOfTheMonth() {
   };
 };
 createDaysOfTheMonth();
+
+// Exercicio 02
+function createButton(string) {
+    let buttonFather = document.querySelector('.buttons-container');
+    let button = document.createElement('button');
+    button.className = 'btn-holiday';
+    button.innerHTML = string;
+    buttonFather.appendChild(button);
+
+}
+createButton('Feriados');
